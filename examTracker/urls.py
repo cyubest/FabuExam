683a03897f7d52aab2f1008c3dd0142f1c3d5a28
DESKTOP-UV1HOGK\Celestine <cyubahironoel@gmail.com>
2021-12-16 01:14:12 +0200

    add changes on url and settings

--- examTracker/urls.py
+++ examTracker/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.i18n import JavaScriptCatalog
 from rest_framework.authtoken.views import obtain_auth_token
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('auth/', obtain_auth_token),
     path('', include('Exam.urls')),
     path('admin/', admin.site.urls),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
